049eb1caed27cd75ea7f37bcab9a00beaeaf4c5b
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2016-09-09 19:01:08 +0200

    PDESO

--- js/asw.common.js
+++ js/asw.common.js
@@ -186,20 +186,25 @@ var aswInit = {
     // returns a common object to initialize tables
     initTableOptions: function (table) {
         var options = {
-            autoWidth: true,
-            preDrawCallback: function () {
-                // Initialize the responsive datatables helper once.
-                if (!responsiveHelper_dt_basic) {
-                    responsiveHelper_dt_basic = new ResponsiveDatatablesHelper($('#' + table), breakpointDefinition);
+            "sDom": "<'dt-toolbar'<'col-xs-12 col-sm-6'f><'col-sm-6 col-xs-12 hidden-xs'l>r>"+
+						"t"+
+						"<'dt-toolbar-footer'<'col-sm-6 col-xs-12 hidden-xs'i><'col-xs-12 col-sm-6'p>>",
+			"autoWidth" : true,
+			"oLanguage": {
+                    "sSearch": '<span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>'
+                },
+			"preDrawCallback" : function() {
+                    // Initialize the responsive datatables helper once.
+                    if (!responsiveHelper_dt_basic) {
+                        responsiveHelper_dt_basic = new ResponsiveDatatablesHelper($('#' + table), breakpointDefinition);
+                    }
+                },
+			"rowCallback" : function(nRow) {
+                    responsiveHelper_dt_basic.createExpandIcon(nRow);
+                },
+			"drawCallback" : function(oSettings) {
+                    responsiveHelper_dt_basic.respond();
                 }
-            },
-            rowCallback: function (nRow) {
-                responsiveHelper_dt_basic.createExpandIcon(nRow);
-            },
-            drawCallback: function (oSettings) {
-                responsiveHelper_dt_basic.respond();
-            },
-            language: datatable_languages['es']
         };
         return options;
     }
